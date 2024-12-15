12824ef243d2230db43a27cc97f9f782ecb5b71e
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2024-12-15 18:02:47 +0100

    added the user component feature

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DUMMY_USERS } from '../../dunmmy-users';
 
 @Component({
   selector: 'app-user',
   imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  selectedUser = DUMMY_USERS[this.randomIndex];
 
+  ngOnInit(): void {
+    this.selectedUser = DUMMY_USERS[this.randomIndex];
+  }
 }
