1fd5be106d3a6b59e78a9a0c14c7a5ffe28581cf
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2024-12-18 12:52:54 +0100

    user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,8 +9,8 @@ import { DUMMY_USERS } from '../../dunmmy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent implements OnInit {
+  selectedUser?: Object;
   private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  selectedUser = DUMMY_USERS[this.randomIndex];
 
   ngOnInit(): void {
     this.selectedUser = DUMMY_USERS[this.randomIndex];
