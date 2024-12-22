3849b644cd127f30fffb11e7cd04e133f410abec
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2024-12-22 15:59:52 +0100

    computed properties added and better componetn identification

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DUMMY_USERS } from '../../dunmmy-users';
+import { User } from '../../models/user.interface';
 
 @Component({
   selector: 'app-user',
@@ -9,8 +10,29 @@ import { DUMMY_USERS } from '../../dunmmy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent implements OnInit {
-  selectedUser?: Object;
+  selectedUser?: User = {
+    avatar: '',
+    name: '',
+    id: '',
+  };
   private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+  get imagePath() {
+    return this.selectedUser ? 'users/' + this.selectedUser?.avatar : '';
+  }
+
+  get userName() {
+    return this.selectedUser ? this.selectedUser?.name : '';
+  }
+
+  get userId() {
+    return this.selectedUser?.id;
+  }
+
+  onSelectUser(e: Event) {
+    let button = <HTMLButtonElement>e.target;
+    console.log('Clicked!', button.id, button.getAttribute('data-userid'));
+  }
 
   ngOnInit(): void {
     this.selectedUser = DUMMY_USERS[this.randomIndex];
