81f4b65bc432781cb0f2a5b101c3eb93ae229a52
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2025-01-17 22:51:32 +0100

    back to old input and property usage

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -16,14 +16,15 @@ import { DUMMY_USERS } from '../../dunmmy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent implements OnInit {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) name!: string;
 
   /* Alternative input definition using input function from Angular. A bit cleaner code. */
-  avatar = input.required<string>();
-  id = input.required<string>();
-  name = input.required<string>();
+
+  // avatar = input.required<string>();
+  // id = input.required<string>();
+  // name = input.required<string>();
 
   selectedUser = signal(DUMMY_USERS[0]);
 
@@ -31,14 +32,14 @@ export class UserComponent implements OnInit {
    as a getter. this property is usable without
    calling the function (like a property as usual)
   */
-  // get imagePath() {
-  //   return 'users/' + this.avatar();
-  // }
+  get imagePath() {
+    return 'users/' + this.avatar;
+  }
 
   /* Using computed values */
-  imagePath = computed(() => {
-    return 'users/' + this.avatar();
-  });
+  // imagePath = computed(() => {
+  //   return 'users/' + this.avatar();
+  // });
 
   ngOnInit(): void {}
 
