4e416e36d9ea39a8f5fb709372c299aebf17e91c
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2025-01-19 18:51:32 +0100

    tasks component introduction

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../../dunmmy-users';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) name!: string;
+
+  onSelectUser(id: string) {}
+}
