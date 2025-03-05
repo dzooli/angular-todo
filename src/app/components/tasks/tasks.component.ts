b16016f86df53d32eeeb4ba46a29cf2ee52c7df1
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2025-03-05 01:27:53 +0100

    add task component and update tasks display logic

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,48 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../../dunmmy-users';
 
+import { TaskComponent } from './task/task.component';
+
 @Component({
   selector: 'app-tasks',
-  imports: [],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
+  imports: [TaskComponent],
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn the basic and advanced features of Angular',
+      completed: false,
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master React',
+      summary: 'Learn React in depth',
+      completed: false,
+      dueDate: '2021-12-31',
+    },
+    {
+      id: 't3',
+      userId: 'u2',
+      title: 'Master Vue',
+      summary: 'To complete the frontend development journey, Learn Vue',
+      completed: false,
+      dueDate: '2022-12-31',
+    },
+  ];
 
   onSelectUser(id: string) {}
+
+  get selecteUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
