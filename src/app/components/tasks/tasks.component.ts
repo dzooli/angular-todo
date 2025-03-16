f5ddf69d280e988e2d196647888bebd99a0f0bfc
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2025-03-16 15:36:53 +0100

    add new task component with form and styling for task creation

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,16 +2,19 @@ import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../../dunmmy-users';
 
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask = false;
 
   tasks = [
     {
@@ -48,5 +51,13 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
